6005b403b856010b8082d52a2603378894708976
unknown <bobby.gerez@yahoo.com>
2018-11-09 16:59:25 +0800

    New Updates

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,8 @@ Route::group(['prefix' => 'auth', 'middleware' => 'jwt.auth'], function () {
     Route::post('logout', 'Auth\LoginController@logout');
 });
 
-Route::get('get-provinces', 'API\Province\ProvinceController@allProvinces');
-Route::get('get-cities/{provCode}', 'API\City\CityController@getCities');
-Route::get('get-brgys/{citymunCode}', 'API\Brgy\BrgyController@getBrgy');
-Route::get('get-branches/{storeId}', 'API\Branch\BranchController@getBranches');
-Route::get('search-user', 'API\User\UserController@search');
-
 Route::resource('roles', 'API\Roles\RoleController');
 Route::resource('user', 'API\User\UserController');
 Route::resource('holdings', 'API\Holding\HoldingController');
 Route::resource('countries', 'API\Country\CountryController');
+Route::resource('regions', 'API\Region\RegionController');
